b8efd99f013791b894a434d089ce8403321d6679
Cheri Portman <cheriportman@hotmail.com>
2019-05-13 14:51:58 -0700

    simple-dogs exercise complete

---
+++ src/components/Dog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Dog() {
+  return (
+    <dl>
+      <dt>Name</dt>
+      <dd>Stitch</dd>
+
+      <dt>Age</dt>
+      <dd>5</dd>
+
+      <dt>Weight</dt>
+      <dd>18 pounds</dd>
+    </dl>
+  );
+}
